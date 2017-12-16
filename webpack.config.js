29cd261977126967cd97429c6f33396347d065cf
Nil Panés <nil.panes@gmail.com>
2017-12-16 22:20:52 +0100

    Added webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})
+  
 module.exports = {
-  entry: './client/index.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve('dist'),
     filename: 'index_bundle.js'
@@ -10,5 +17,6 @@ module.exports = {
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
-  }
+  },
+  plugins: [HtmlWebpackPluginConfig] 
 }
